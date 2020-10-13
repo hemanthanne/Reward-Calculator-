135aa1664d1ab5f3e7edb236e4392cf1c89fc88c
hemanthanne <72768101+hemanthanne@users.noreply.github.com>
2020-10-13 09:07:27 -0500

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
+import ReactTable from 'react-table';
 import './App.css';
 
 function rewardsResults(incomingData) {
